6bc21573fc151e6a417315989eb62e35cc672a5e
Parth Rawat <parthpriyanshu10@gmail.com>
2023-10-26 22:01:04 +0530

    Corrected Routing file

--- src/Routing.java
+++ src/Routing.java
@@ -1,71 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-<<<<<<< HEAD
 import java.sql.SQLOutput;
-import java.util.*;
-
-
-
-public class Main {
-
-    static void Create_Bus_Map(Main g)
-{
-    g.addVertex("ISBT");
-    g.addVertex("Jolly Grant Airport");
-    g.addVertex("Kargi Chowk");
-    g.addVertex("Rispana Pull");
-    g.addVertex("Clock Tower");
-    g.addVertex("Jogiwala");
-    g.addVertex("Mokhampur");
-    g.addVertex("Ballupur Chowk");
-    g.addVertex("IMA");
-    g.addVertex("FRI");
-    g.addVertex("Pacific Mall");
-    g.addVertex("Prem Nagar");
-    g.addVertex("Survey Chowk");
-    g.addVertex("Rajpur Road");
-    g.addVertex("Sai Mandir");
-    g.addVertex("IT Park");
-    g.addVertex("Railway Station");
-    g.addVertex("Musoorie Diversion");
-    g.addVertex("Saharanpur Chowk");
-    g.addVertex("Prince Chowk");
-    g.addVertex("DRDO");
-    g.addVertex("Nanda Ki Chowki");
-    g.addVertex("Balliwala Chowk");
-    g.addVertex("Shimla Bypass");
-
-    g.addEdge("ISBT", "Jolly Grant Airport", 8);
-    g.addEdge("Jolly Grant Airport", "Kargi Chowk", 8);
-    g.addEdge("Kargi Chowk", "Rispana Pull", 8);
-    g.addEdge("Rispana Pull", "Clock Tower", 8);
-    g.addEdge("Clock Tower", "Jogiwala", 8);
-    g.addEdge("Jogiwala", "Mokhampur", 8);
-    g.addEdge("Mokhampur", "Ballupur Chowk", 8);
-    g.addEdge("Ballupur Chowk", "IMA", 8);
-    g.addEdge("IMA", "FRI", 8);
-    g.addEdge("FRI", "Pacific Mall", 8);
-    g.addEdge("Pacific Mall", "Prem Nagar", 8);
-    g.addEdge("Prem Nagar", "Survey Chowk", 8);
-    g.addEdge("Survey Chowk", "Rajpur Road", 8);
-    g.addEdge("Rajpur Road", "Sai Mandir", 8);
-    g.addEdge("Sai Mandir", "IT Park", 8);
-    g.addEdge("IT Park", "Railway Station", 8);
-    g.addEdge("Railway Station", "Musoorie Diversion", 8);
-    g.addEdge("Musoorie Diversion", "Saharanpur Chowk", 8);
-    g.addEdge("Saharanpur Chowk", "Prince Chowk", 8);
-    g.addEdge("Prince Chowk", "DRDO", 8);
-    g.addEdge("DRDO", "Nanda Ki Chowki", 8);
-    g.addEdge("Nanda Ki Chowki", "Balliwala Chowk", 8);
-    g.addEdge("Balliwala Chowk", "Shimla Bypass", 8);
-}
-
-    public static void main(String[] args) throws IOException 
-    {
-        Main g = new Main();
-        Create_Metro_Map(g);
-=======
 import java.util.*;
 
 
@@ -140,8 +76,62 @@ public class Routing {
         vtx2.nbrs.put(vname1, value);
     }
 
+    static void Create_Bus_Map(Routing g)
+    {
+        g.addVertex("ISBT");
+        g.addVertex("Jolly Grant Airport");
+        g.addVertex("Kargi Chowk");
+        g.addVertex("Rispana Pull");
+        g.addVertex("Clock Tower");
+        g.addVertex("Jogiwala");
+        g.addVertex("Mokhampur");
+        g.addVertex("Ballupur Chowk");
+        g.addVertex("IMA");
+        g.addVertex("FRI");
+        g.addVertex("Pacific Mall");
+        g.addVertex("Prem Nagar");
+        g.addVertex("Survey Chowk");
+        g.addVertex("Rajpur Road");
+        g.addVertex("Sai Mandir");
+        g.addVertex("IT Park");
+        g.addVertex("Railway Station");
+        g.addVertex("Musoorie Diversion");
+        g.addVertex("Saharanpur Chowk");
+        g.addVertex("Prince Chowk");
+        g.addVertex("DRDO");
+        g.addVertex("Nanda Ki Chowki");
+        g.addVertex("Balliwala Chowk");
+        g.addVertex("Shimla Bypass");
+
+        g.addEdge("ISBT", "Jolly Grant Airport", 8);
+        g.addEdge("Jolly Grant Airport", "Kargi Chowk", 8);
+        g.addEdge("Kargi Chowk", "Rispana Pull", 8);
+        g.addEdge("Rispana Pull", "Clock Tower", 8);
+        g.addEdge("Clock Tower", "Jogiwala", 8);
+        g.addEdge("Jogiwala", "Mokhampur", 8);
+        g.addEdge("Mokhampur", "Ballupur Chowk", 8);
+        g.addEdge("Ballupur Chowk", "IMA", 8);
+        g.addEdge("IMA", "FRI", 8);
+        g.addEdge("FRI", "Pacific Mall", 8);
+        g.addEdge("Pacific Mall", "Prem Nagar", 8);
+        g.addEdge("Prem Nagar", "Survey Chowk", 8);
+        g.addEdge("Survey Chowk", "Rajpur Road", 8);
+        g.addEdge("Rajpur Road", "Sai Mandir", 8);
+        g.addEdge("Sai Mandir", "IT Park", 8);
+        g.addEdge("IT Park", "Railway Station", 8);
+        g.addEdge("Railway Station", "Musoorie Diversion", 8);
+        g.addEdge("Musoorie Diversion", "Saharanpur Chowk", 8);
+        g.addEdge("Saharanpur Chowk", "Prince Chowk", 8);
+        g.addEdge("Prince Chowk", "DRDO", 8);
+        g.addEdge("DRDO", "Nanda Ki Chowki", 8);
+        g.addEdge("Nanda Ki Chowki", "Balliwala Chowk", 8);
+        g.addEdge("Balliwala Chowk", "Shimla Bypass", 8);
+    }
+
     public static void main(String[] args) throws IOException {
->>>>>>> 099827470e3e4329a8c80da8b716484277d3729d
+
+        Routing g = new Routing();
+        Create_Bus_Map(g);
 
         System.out.println("\n\t\t\t***WELCOME TO THE ELECTRIC BUS MAP***");
 
@@ -178,10 +168,6 @@ public class Routing {
             }
         }
 
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 099827470e3e4329a8c80da8b716484277d3729d
         }
     }
